7b17751f68103a1888460c8fab4832730207f407
hitesh920 <hiteshkamble920@gmail.com>
2021-07-10 15:20:49 +0000

    CPH1859: pixy-ify

--- pixys_CPH1859.mk
+++ pixys_CPH1859.mk
@@ -20,7 +20,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common LOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/pixys/config/common_full_phone.mk)
 
 # Inherit from oppo CPH1861
 $(call inherit-product, device/oppo/CPH1859/device.mk)
@@ -34,7 +34,10 @@ TARGET_SCREEN_WIDTH := 1080
 TARGET_SCREEN_HEIGHT := 2160
 TARGET_BOOT_ANIMATION_RES := 1080
 
-PRODUCT_NAME := lineage_CPH1859
+# Gapps
+TARGET_GAPPS_ARCH := arm64
+
+PRODUCT_NAME := pixys_CPH1859
 PRODUCT_DEVICE := CPH1859
 PRODUCT_MANUFACTURER := OPPO
 PRODUCT_BRAND := OPPO
@@ -50,4 +53,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="CPH1859" \
     PRIVATE_BUILD_DESC="full_oppo6771_17065-user 9 PPR1.180610.011 eng.root.20200509.060522 release-keys"
 
-BUILD_FINGERPRINT := "google/flame/flame:S/SPB2.210513.007/7398525:user/release-keys"
+BUILD_FINGERPRINT := "google/flame/flame:11/RQ3A.210705.001/7380771:user/release-keys"
